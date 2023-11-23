d348a0695ea30e1617ffd1004c49854138df1322
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2023-11-23 20:50:43 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Board from '@/app/ui/board';
 export default function Home() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
+  const [isAscending, setIsAscending] = useState(true);
   const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
@@ -17,6 +18,10 @@ export default function Home() {
 
   function jumpTo(nextMove: number) {
     setCurrentMove(nextMove);
+  }
+
+  function sortMoves() {
+    setIsAscending(!isAscending);
   }
 
   const moves = history.map((_, move) => {
@@ -38,10 +43,12 @@ export default function Home() {
     <div className="game">
       <div className="game-board">
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+        <br />
+        <button onClick={() => sortMoves()}>Toggle the sort order</button>
       </div>
       <div className="game-info">
-        <ol>{moves}</ol>
+        <ol>{isAscending ? moves : moves.reverse()}</ol>
       </div>
-    </div>
+    </div >
   );
 }
